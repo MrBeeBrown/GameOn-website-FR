63dd6a84cc237236bc4216a1c0234af9b7a61b0f
beebrown <bruno.grosdesirs@outlook.fr>
2023-03-20 13:03:07 +0100

    maj

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,23 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+//***select the close icon from modal***
+const modalClose = document.querySelector(".close");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+//***close modal event***
+modalClose.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+//***close modal form***
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
